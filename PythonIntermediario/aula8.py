b371190471df5d917b01b122350cb5439619f1ec
Armando Marioto <armandomarioto@gmail.com>
2021-06-07 13:51:58 -0300

    a

---
+++ PythonIntermediario/aula8.py
@@ -0,0 +1,14 @@
+'''
+Dicionarios em Python
+'''
+
+
+d1 = {'chave1':'Valor da Chave'} #  quando se usa {} está criando um dicionario
+d2 = dict(chave2='Valor da Chave2', chave_d2 = 'Valor da chave da D2')
+d1['nova_chave'] = 'Valor da nova chave'
+print(d1)
+print(d2)
+print(d1['chave1'])
+print(d1['nova_chave'])
+print(d2['chave_d2'])
+
