443894383015bb25ac7ee5941a19126de86df27b
Armando Marioto <armandomarioto@gmail.com>
2021-06-11 10:31:36 -0300

    Exercicio 4

--- PythonIntermediario/Exercicio4.py
+++ PythonIntermediario/Exercicio4.py
@@ -1,6 +1,14 @@
-lista_a = [1,2,3,4,5]
-lista_b = [1,2,3,4,5]
+from itertools import zip_longest
 
-lista_soma = [x +y for x,y in zip(lista_a,lista_b)]
+lista_a = [10, 2, 3, 4, 5]
+lista_b = [12, 2, 3, 6, 50, 60, 70]
+lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
+print(lista_soma)  # [22, 4, 6, 10, 55, 60, 70]
 
-print(lista_soma)
+
+# lista_a = [1,2,3,4,5]
+# lista_b = [1,2,3,4,5]
+#
+# lista_soma = [x +y for x,y in zip(lista_a,lista_b)]
+#
+# print(lista_soma)
