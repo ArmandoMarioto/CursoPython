8ecbbe2276243069e3e34cc0f518c51605eb8351
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-15 10:26:09 -0300

    Aula 20 - Try , except - Tratando exceções em python

--- PythonIntermediario/aula20.py
+++ PythonIntermediario/aula20.py
@@ -1,8 +1,18 @@
 '''
-Aula 19 - Try , except - Tratando exceções em python
+Aula 20 - Try , except - Tratando exceções em python
 '''
 
 try:
-    print(a)
+    print()
 except NameError as erro:
     print('Erro', erro)
+except IndexError as erro:
+    print('Erro', erro)
+except Exception as erro:
+    print('Erro', erro)
+else:
+    print('Excecuta sempre que o bloco Try com executado sem erro.')
+finally:
+    print('Executa independente se ocorreu erro ou não no TRY.')
+
+print('bora continuar')
