792f7307aa18b5a53b2465223019f837acb01b8d
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-15 09:55:05 -0300

    Aula 19 - Filter

--- PythonIntermediario/aula18mapeamento.py
+++ PythonIntermediario/aula18mapeamento.py
@@ -16,7 +16,28 @@ from aula18Dados import produtos, pessoas, lista
 # for produto in novos_produtos:
 #     print(produto)
 
+#
+# nomes = map(lambda p: p['nome'],pessoas)
+# for p in nomes:
+#     print(p)
 
-nomes = map(lambda p: p['nome'],pessoas)
-for p in nomes:
-    print(p)
+
+# nova_lista = filter(lambda x: x > 5,lista)  #  Com função filter
+# nova_lista = [x for x in lista if x > 5]  #  Com list comprehension
+# print(list(nova_lista))
+
+
+# nova_produtos = filter(lambda x : x['preco']>10,produtos)
+#
+# for produto in nova_produtos:
+#     print(produto)
+
+def filtra(pessoa):
+    if pessoa['idade'] <= 18:
+        return True
+    return False
+
+nova_lista = filter(filtra,pessoas)
+
+for pessoa in nova_lista:
+    print(pessoa)
