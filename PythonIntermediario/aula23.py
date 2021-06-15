ad0c2765470f17f6d01c6e1ea49512fa7310693d
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-15 11:08:13 -0300

    Aula 23 - Modolus padrao do Python

---
+++ PythonIntermediario/aula23.py
@@ -0,0 +1,16 @@
+'''
+Aula 23 - Modolus padrao do Python
+modulos sao arquivos .py (que contem codigo python) e servem para expandir
+as funcionalidades padrao da linguagem
+'''
+
+# import sys
+# print(sys.platform)
+
+# from sys import platform
+# print(platform)
+
+from random import randint
+
+for i in range(10):
+    print(randint(0,10))
