e5b5b9d0ac55d23e2e328424918a1f364e8759ec
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-17 18:03:10 -0300

    Aula 1 - POO

--- PythonPOO/Aula1/aula1.py
+++ PythonPOO/Aula1/aula1.py
@@ -1,4 +1,8 @@
-import PythonPOO.Aula1.pessoa as Pessoa
 
-p1 = Pessoa()
-p1 = Pessoa()
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+p2 = Pessoa('João', 32)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
