8335fffca5f9139d6fc0a319c13bdbcb706412a9
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-20 10:59:41 -0300

    Aula 5 - Encapsulamento e Associação

---
+++ PythonPOO/aula5/main.py
@@ -0,0 +1,18 @@
+'''
+Aula 5 - Encapsulamento e Associação
+'''
+
+from classes import Escritor
+from classes import Caneta
+from classes import MaquinaDeEscrever
+
+escritor = Escritor('Joãozinho')
+caneta = Caneta('Bic')
+maquina = MaquinaDeEscrever()
+
+escritor.ferramenta = maquina
+escritor.ferramenta.escrever()
+
+del escritor
+print(caneta.marca)
+maquina.escrever()
