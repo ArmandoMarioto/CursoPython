dfc32977d1e2e9eb95af31f3e57d9a84604519a2
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-21 11:09:03 -0300

    Aula 10 - Herenças multiplas

---
+++ PythonPOO/Aula10/main.py
@@ -0,0 +1,16 @@
+'''
+    Aula 10 - Herenças multiplas
+'''
+from smartphone import Smartphone
+
+smartphone = Smartphone('Pocophone F1')
+smartphone.conetar()
+smartphone.desligar()
+smartphone.ligar()
+smartphone.conetar()
+smartphone.conetar()
+smartphone.conetar()
+smartphone.desligar()
+smartphone.conetar()
+smartphone.desconectar()
+smartphone.desconectar()
