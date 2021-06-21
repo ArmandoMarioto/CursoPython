7e17c19365ed9c1a8b02d09117c18b15cc761f6f
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-21 11:37:42 -0300

    Aula 11 -  Conceito de Classe Abstratas

---
+++ PythonPOO/Aula11/classes/cp.py
@@ -0,0 +1,11 @@
+from CursoPython.PythonPOO.Aula11.classes.conta import Conta
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        if self.saldo < valor:
+            print('Saldo insuficiente')
+            return
+
+        self.saldo -= valor
+        self.detalhes()
