5f957aae0a6f258e2b99d28eb6ac7f2ac71ef2dd
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-21 10:20:36 -0300

    Aula 9 - Sobreposição de membros

--- PythonPOO/Aula8/classes.py
+++ PythonPOO/Aula8/classes.py
@@ -10,6 +10,25 @@ class Pessoa:  # super classe
 class Cliente(Pessoa):# herdando da super,Cliente é classe filha
     def comprar(self):
         print(f'{self.nome} está comprando...')
+
+    def falar(self):
+        print('Estou em CLIENTE.')
+
+class ClienteVIP(Cliente):#Sobrepondo o metodo falar
+    def __init__(self,nome, idade,sobrenome):
+        super().__init__(nome,idade)
+        self.sobrenome = sobrenome
+
+
+
+    def falar(self):
+        #  super().falar()# usando o metodo da Super classe.
+        Pessoa.falar(self)# usando o metodo da classe Pessoa.
+        Cliente.falar(self)# usando o metodo da classe Cliente.
+        print(f'{self.nome} {self.sobrenome} está falando.......')
+
+
+
 class Aluno(Pessoa):# herdando da super,Aluno é classe filha
     def estudar(self):
         print(f'{self.nome} está estudando....')
--- PythonPOO/Aula8/main.py
+++ PythonPOO/Aula8/main.py
@@ -1,14 +1,19 @@
 '''
     Aula 8 - Herança simples
+    Aula 9 - Sobreposição de membros
 '''
 from classes import *
 
-c1 = Cliente('Luiz',45)
-print(c1.nome)
-c1.falar()
-c1.comprar()
-print('\n ##########################\n')
-a1 = Aluno('Armando',25)
-print(a1.nome)
-a1.falar()
-a1.estudar()
+# c1 = Cliente('Luiz',45)
+# print(c1.nome)
+# c1.falar()
+# c1.comprar()
+# print('\n ##########################\n')
+# a1 = Aluno('Armando',25)
+# print(a1.nome)
+# a1.falar()
+# a1.estudar()
+
+
+c2 = ClienteVIP('Armando',25,'Marioto')
+c2.falar()
